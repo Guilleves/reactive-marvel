85d3ff635c31216639aa5165b125f563f8d4242c
guillermina <guilleves@gmail.com>
2016-10-14 13:59:56 -0300

    Add fetch series by year

--- scripts/marvel_api.js
+++ scripts/marvel_api.js
@@ -36,5 +36,17 @@ var MarvelApi = {
         xhr.open("GET", "http://gateway.marvel.com:80/v1/public/characters/" + charId +"/comics?ts=1&limit=100&apikey=c853e32a3cdaa00bfb344c5bd47c1230&hash=ad8228ded3fe3fd80db2d703b88f1397");
         xhr.send();
     },
+    seriesByYear: function(year, fn) {
+        let xhr = new XMLHttpRequest();
+        console.log(year);
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var chars = JSON.parse(this.responseText);
+                return fn(chars)
+            }
+        };
+        xhr.open("GET", "http://gateway.marvel.com/v1/public/series?startYear="+ year + "&ts=1&apikey=c853e32a3cdaa00bfb344c5bd47c1230&hash=ad8228ded3fe3fd80db2d703b88f1397");
+        xhr.send();
+    },
 }
 export default MarvelApi;
